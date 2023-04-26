9fd5a510a0fd86bd4c0aac4431065c4888e81e2d
Sanechek1337 <rednaxela659@gmail.com>
2023-04-26 23:56:22 +0600

    added Routes

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,10 +1,24 @@
-import PeoplePage from '@containers/PeoplePage';
+import { Route, Routes } from 'react-router-dom';
+import routesConfig from '@routes/routesConfig';
+
+import Header from '@components/Header/Header';
 
 import styles from './App.module.css';
 
 const App = () => {
 	return (
-		<PeoplePage />
+		<div className={styles.wrapper}>
+			<Header />
+			<Routes>
+				{routesConfig.map((route, index) => (
+					<Route
+						key={index}
+						path={route.path}
+						element={route.element}
+					/>
+				))}
+			</Routes>
+		</div>
 	)
 }
 
